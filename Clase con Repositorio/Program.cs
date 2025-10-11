b78937aa4fee90ae3e4218ac3dbc813266a1cce4
ZNorFer GoNav <ZNorFer GoNav@ZNORFER>
2025-10-11 12:08:15 -0600

    Agregar archivos de proyecto.

---
+++ Clase con Repositorio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_con_Repositorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
